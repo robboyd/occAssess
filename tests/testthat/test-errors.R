7e24b781f332b8d5e51d5ce37a3444d0ecad328a
Rob Boyd <robboy@ceh.ac.uk>
2021-04-12 15:27:41 +0100

    update tests and drop separate set for assessEnvBias

--- tests/testthat/test-errors.R
+++ tests/testthat/test-errors.R
@@ -3,17 +3,19 @@ library(testthat)
 
 context("errors and warnings")
 
-data(random)
+data("random40Species")
 
 periods <- list(2000:2004, 2005:2010)
 
 test_that("check basic errors are caught", {
   
-  expect_error( assessSpatialUncertainty(dat = random[, -1], periods = periods) )
+  expect_error( assessRecordNumber(dat = random40Species[, -1], periods = periods) )
   
-  random$identifier[10] <- NA
+  expect_error( assessRecordNumber(dat = random40Species[dat$species == "human", ], periods = periods) )
   
-  expect_error( assessSpatialUncertainty(dat = random, periods = periods))
+  random40Species$identifier[10] <- NA
+  
+  expect_error( assessRecordNumber(dat = random40Species, periods = periods))
   
 })
 
--- tests/testthat/test-outputs.R
+++ tests/testthat/test-outputs.R
@@ -1,24 +1,27 @@
 library(occAssess)
-library(ggfortify)
-library(ggplot2)
 
 context("test outputs")
 
-data(random)
+data("random40Species")
 
-periods <- list(2000:2004, 2005:2010)
+data("backgroundEnvDat")
+
+data("random40SpeciesEnvDat")
+
+periods <- list(2001:2002, 2003:2004, 2005:2006, 2007:2008, 2009:2010)
 
 test_that("check outputs are as expected", {
   
-  expect_equal( assessSpatialUncertainty(dat = random, periods = periods)$data[1,1], 1 )
+  expect_equal( assessRecordNumber(dat = random40Species, periods = periods)$data$val[1], 3968 )
   
-  expect_equal( assessRarityBias(dat = random, periods = periods, res = 10000)$data$index[1], 0.05389407 )
+  expect_equal( assessRarityBias(dat = random40Species, 
+                                 periods = periods, 
+                                 res = 10000,
+                                 prevPerPeriod = TRUE)$data$index[1], 0.9992043 )
   
-  expect_equal( assessSpeciesNumber(dat = random, periods = periods)$data$val[1], 5 )
+  expect_equal( assessSpeciesNumber(dat = random40Species, periods = periods)$data$val[1], 40 )
   
-  expect_equal( assessSpeciesID(dat = random, periods = periods, type = "proportion")$data$prop[1], 1 )
-  
-  expect_equal( length(assessEnvBias(dat = random, periods = periods, nEnvVar = 19)$pca[[1]]), 19 )
+  expect_equal( assessSpeciesID(dat = random40Species, periods = periods, type = "proportion")$data$prop[1], 1 )
   
 })
 
