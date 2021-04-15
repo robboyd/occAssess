d8907bb09cc27d508bff66b76557ab0d15f62123
Rob Boyd <robboy@ceh.ac.uk>
2021-04-15 14:47:41 +0100

    documentation for data used in reproducible vignette

---
+++ R/UKWGS84mask.R
@@ -0,0 +1,16 @@
+#' Example mask layer for assessSpatialBias(). .
+#'
+#' Raster layer defining UK with finite values and elsewhere as NA. 
+#'
+#' @docType data
+#'
+#' @usage data(UKWGS84mask)
+#'
+#' @format An object of class \code{"data.frame"}
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(UKWGS84mask)
+
+"UKWGS84mask"
---
+++ R/simDat.R
@@ -0,0 +1,19 @@
+#' Simulated occurrence data.
+#'
+#' Simulate occurrence data for 11 species in the UK. The data are drawn from a weighted random sample across the UK, where 
+#' the weights are equal to predicted species' distributions predicted in another project. For each simulated species, the 
+#' weights are taken from an anonymous actual species' predicted distributions. The data are generated uniformly over the period
+#' 2001 to 2010.
+#'
+#' @docType data
+#'
+#' @usage data(simDat)
+#'
+#' @format An object of class \code{"data.frame"}
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(simDat)
+
+"simDat"
