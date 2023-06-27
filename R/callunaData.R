d52f1296e0e66f5cdfc7252285d1beed95eb0fc1
Robin Boyd <robboy@ceh.ac.uk>
2023-06-27 13:34:57 +0100

    document calluna data

---
+++ R/callunaData.R
@@ -0,0 +1,16 @@
+#' Arabidopsis QTL data on gravitropism
+#'
+#' Data on Calluna vulgaris occupancy in Britain. Each row represents 1 1x1 km grid square. There are columns 
+#' for sample inclusion in two time-periods (1=sampled and =0 not sampled), occupancy (1=occupued and 0= not occupued) and several
+#' auxiliary variables.
+#'
+#' @docType data
+#'
+#' @usage data(callunaData)
+#'
+#' @format An object of class \code{"data.frame"}.
+#'
+#' @keywords datasets
+#'
+#' @references Boyd et al. (2023) EcoEvoRxiv https://doi.org/10.32942/X2359P
+
